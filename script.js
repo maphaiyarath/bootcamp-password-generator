3906bfae349aaec7f4b6df09ed5b8b8b8a1274ae
chelle <maphaiyarath@gmail.com>
2020-10-12 21:13:17 -0500

    added comments

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ var alphaChars = 'abcdefghijklmnopqrstuvwxyz';
 var numChars = '0123456789';
 var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-// Assignment Code
+// password is generated that matches the selected criteria
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   alert('Welcome to the Password Generator!');
+
+  // user can choose 
+  // ask user to select which criteria to include in the password
   var lower = confirm('Would you like lowercase letters?');
   var upper = confirm('Would you like uppercase letters?');
   var numeric = confirm('Would you like numeric characters?');
@@ -14,43 +17,47 @@ function generatePassword() {
   var passwordSet = '';
 
   if (!lower && !upper && !numeric && !special) {
-    alert('Generate a password by selecting at least one of the criteria.');
+    alert('Generate a password by selecting at least one type of character.');
   } else {
+    // user can choose a length of at least 8 characters and no more than 128 characters
     var length = prompt('How long would you like your password to be? Choose from 8 to 128 characters.');
-    if (lower) {
-      // passwordSet.push(alphaChars);
-      passwordSet += alphaChars;
-    }
-  
-    if (upper) {
-      // passwordSet.push(alphaChars.toUpperCase());
-      passwordSet += alphaChars.toUpperCase();
-    }
+
+    if (length < 8 || length > 128) {
+      alert('Generate a password with a length of at least 8 characters and no more than 128 characters.');
+    } else {
+      // lowercase, uppercase, numeric, and/or special characters are added to possible password character set
+      if (lower) {
+        passwordSet += alphaChars;
+      }
     
-    if (numeric) {
-      // passwordSet.push(numChars);
-      passwordSet += numChars;
-    }
-  
-    if (special) {
-      // passwordSet.push(specialChars);
-      passwordSet += specialChars;
-    }
-  
-    var pw = '';
+      if (upper) {
+        passwordSet += alphaChars.toUpperCase();
+      }
+      
+      if (numeric) {
+        passwordSet += numChars;
+      }
     
-    for (var i = 0; i < length; i++) {
-      var random = Math.floor(Math.random() * passwordSet.length);
-      pw += passwordSet[random];
+      if (special) {
+        passwordSet += specialChars;
+      }
+    
+      var pw = '';
+      console.log(passwordSet);
+      
+      for (var i = 0; i < length; i++) {
+        var random = Math.floor(Math.random() * passwordSet.length);
+        pw += passwordSet[random];
+      }
+    
+      return pw;
     }
-  
-    return pw;
   }
 
   return null;
 }
 
-// Write password to the #password input
+// the password is written to the page through the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -59,5 +66,8 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// TODO
+// input should be validated and at least one character type should be selected
