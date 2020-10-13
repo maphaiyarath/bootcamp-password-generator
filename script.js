b1616897ee3847fe56a4157a1de96bceedd200c0
chelle <maphaiyarath@gmail.com>
2020-10-12 19:25:24 -0500

    added prompts for pw requirements

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+var alphaChars = 'abcdefghijklmnopqrstuvwxyz';
+var numChars = '0123456789';
+var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  alert('Welcome to the Password Generator!');
+  var lower = confirm('Would you like lowercase letters?');
+  var upper = confirm('Would you like uppercase letters?');
+  var numeric = confirm('Would you like numeric characters?');
+  var special = confirm('Would you like special characters?');
+  
+  var passwordSet = []
+
+  if (lower) {
+    passwordSet.push(alphaChars);
+  }
+
+  if (upper) {
+    passwordSet.push(alphaChars.toUpperCase());
+  }
+  
+  if (numeric) {
+    passwordSet.push(numChars);
+  }
+
+  if (special) {
+    passwordSet.push(specialChars)
+  }
+  console.log(passwordSet);
+}
 
 // Write password to the #password input
 function writePassword() {
