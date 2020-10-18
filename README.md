6e5416e1cb512a5baadda80374feec125fc099eb
chelle <maphaiyarath@gmail.com>
2020-10-17 20:57:02 -0500

    updated readme with screenshots and info

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# bootcamp-password-generator
+# Javascript: Password Generator
+
+Click the button:
+![Password Generator](./assets/pic1.png)
+
+Select some criteria:
+![Select Criteria](./assets/pic2.png)
+
+And voilà!
+![Voilà!](./assets/pic3.png)
+
+You can see the deployed app [here](https://maphaiyarath.github.io/password-generator/).
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Badges](#badges)
+
+## Description
+This application allows you to generate a random password based on selected criteria, such as length, lowercase, uppercase, numeric, and/or special characters. Stronger passwords provide greater security. Simply click the button to generate a secure password!
+
+Further development could include having an icon that would allow you to copy to your clipboard. Also, you are not guaranteed to have all of the criteria you select for your password.
+
+## Installation
+n/a
+
+## Usage
+n/a
+
+## Credits
+n/a
+
+## License
+This project is licensed under the MIT license.
+
+## Badges
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/maphaiyarath/password-generator)
--- script.js
+++ script.js
@@ -7,63 +7,62 @@ var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // ask user to select which criteria to include in the password
-  var lower = confirm('Would you like lowercase letters?');
-  var upper = confirm('Would you like uppercase letters?');
-  var numeric = confirm('Would you like numeric characters?');
-  var special = confirm('Would you like special characters?');
+    // ask user to select which criteria to include in the password
+    var lower = confirm('Would you like lowercase letters?');
+    var upper = confirm('Would you like uppercase letters?');
+    var numeric = confirm('Would you like numeric characters?');
+    var special = confirm('Would you like special characters?');
 
-  if (!lower && !upper && !numeric && !special) {
-    alert('Try again - please select at least one type of character.');
-  } else {
-    // user can choose a length of at least 8 characters and no more than 128 characters
-    var length = prompt('How long would you like your password to be? Choose from 8 to 128 characters.');
-
-    if (isNaN(length) || length < 8 || length > 128) {
-      alert('Try again - please select a valid length of at least 8 characters and no more than 128 characters.');
+    if (!lower && !upper && !numeric && !special) {
+        alert('Try again - please select at least one type of character.');
     } else {
-      var passwordSet = '';
+        // user can choose a length of at least 8 characters and no more than 128 characters
+        var length = prompt('How long would you like your password to be? Choose from 8 to 128 characters.');
 
-      // lowercase, uppercase, numeric, and/or special characters are added to possible password character set
-      if (lower) {
-        passwordSet += alphaChars;
-      }
-    
-      if (upper) {
-        passwordSet += alphaChars.toUpperCase();
-      }
-      
-      if (numeric) {
-        passwordSet += numChars;
-      }
-    
-      if (special) {
-        passwordSet += specialChars;
-      }
-    
-      // initialize empty string to add to pw
-      var pw = '';
-      
-      // randomly select character from applicable set
-      for (var i = 0; i < length; i++) {
-        var random = Math.floor(Math.random() * passwordSet.length);
-        pw += passwordSet[random];
-      }
-    
-      return pw;
+        if (isNaN(length) || length < 8 || length > 128) {
+            alert('Try again - please select a valid length of at least 8 characters and no more than 128 characters.');
+        } else {
+            // initialize empty strings for password character set and actual password
+            var passwordSet = '';
+            var pw = '';
+
+            // lowercase, uppercase, numeric, and/or special characters are added to possible password character set
+            if (lower) {
+                passwordSet += alphaChars;
+            }
+
+            if (upper) {
+                passwordSet += alphaChars.toUpperCase();
+            }
+
+            if (numeric) {
+                passwordSet += numChars;
+            }
+
+            if (special) {
+                passwordSet += specialChars;
+            }
+
+            // randomly select character from applicable set
+            for (var i = 0; i < length; i++) {
+                var random = Math.floor(Math.random() * passwordSet.length);
+                pw += passwordSet[random];
+            }
+
+            return pw;
+        }
     }
-  }
 
-  // return null if user does not select any criteria or valid length
-  return null;
+    // return null if user does not select any criteria or valid length
+    return null;
 }
 
 // the password is written to the page through the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 }
 
 // add event listener to generate button which generates series of prompts for password criteria
